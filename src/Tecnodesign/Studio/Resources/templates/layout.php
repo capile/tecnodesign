c98908d1311d5bffda831baff7e0e9954a938c66
Guilherme Capilé <capile@tecnodz.com>
2018-06-05 12:39:57 -0300

    layout updates

--- src/Tecnodesign/Studio/Resources/templates/layout.php
+++ src/Tecnodesign/Studio/Resources/templates/layout.php
@@ -8,16 +8,47 @@
  * @copyright    Tecnodesign (c) 2014
  */
 if(!$title && isset(tdz::$variables['variables']['title'])) $title = tdz::$variables['variables']['title'];
-if(!isset($script)) $script = array();
-else if(!is_array($script)) $script = array($script);
-ksort($script);
-$script = tdz::minify($script, false, true, false);
 
-if(!isset($style)) $style = array();
-else if(!is_array($style)) $style = array($style);
-ksort($style);
-$style = tdz::minify($style);
+if(isset($script)) {
+	$js = '';
+    if(!is_array($script)) $script = explode(',', $script);
+    foreach($script as $k=>$v) {
+        if(is_string($k)) {
+            $js .= tdz::minify($v, TDZ_DOCUMENT_ROOT, true, true, false, '/_/'.$k.'.js');
+            unset($script[$k]);
+        }
+        unset($k, $v);
+    }
+    if($script) {
+        $js .= tdz::minify($script);
+    }
+	$nonce = base64_encode(openssl_random_pseudo_bytes(10));
+	header("Content-Security-Policy: default-src 'none'; style-src 'self' 'unsafe-inline' https:; img-src 'self' https: data:; font-src 'self'; script-src 'nonce-{$nonce}' 'strict-dynamic' 'self'; form-action 'self'; media-src 'self' *.first.org; connect-src 'self' https://api.first.org; object-src 'none'; frame-src https:; frame-ancestors 'none'; base-uri 'self'");
+	$js = str_replace('<script', '<script nonce="'.$nonce.'"', $js);
+    $script = $js;
+    unset($js);
+} else {
+	$script = '';
+}
 
+if(isset($style)) {
+	$css = '';
+    if(!is_array($style)) $style = explode(',', $style);
+    foreach($style as $k=>$v) {
+        if(is_string($k)) {
+            $css .= tdz::minify($v, TDZ_DOCUMENT_ROOT, true, true, false, '/_/'.$k.'.css');
+            unset($style[$k]);
+        }
+        unset($k, $v);
+    }
+    if($style) {
+        $css .= tdz::minify($style);
+    }
+    $style = $css;
+    unset($css);
+} else {
+	$style = '';
+}
 
 if(!isset($content)) {
     if(isset($data)) $content = $data;
