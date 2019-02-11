9b8bf9b65acc26d8286aa972cda5cdc89af32d50
Rodrigo Capilé <rcapile@gmail.com>
2019-02-11 16:13:36 -0200

    More YAML test and travis config

--- src/Tecnodesign/Yaml.php
+++ src/Tecnodesign/Yaml.php
@@ -15,6 +15,9 @@
  * @link      http://tecnodz.com/
  */
 
+/**
+ * @todo is it safe to remove?
+ */
 Tecnodesign_Yaml::parser();
 
 /**
@@ -31,7 +34,24 @@ Tecnodesign_Yaml::parser();
  */
 class Tecnodesign_Yaml
 {
-    public static $parser, $cache = true;
+    /**
+     * Current parser
+     * Leaving it public you allow user to use a parser not compatible with this class
+     * @deprecated You should use setParser if not using native PHP YML
+     * @var string
+     */
+    public static $parser;
+
+    /**
+     * Current parser
+     * @var string
+     */
+    private static $currentParser;
+
+    /**
+     * @var boolean
+     */
+    public static $cache = true;
 
     private static $autoInstall = true;
 
@@ -49,14 +69,14 @@ class Tecnodesign_Yaml
             if (!in_array($parser, [self::PARSE_NATIVE, self::PARSE_SPYC], true)) {
                 throw new \InvalidArgumentException("Invalid parser: $parser");
             }
-            self::$parser = $parser;
-        } elseif (function_exists('yaml_parse')) {
-            self::$parser = self::PARSE_NATIVE;
-        } else {
-            self::$parser = self::PARSE_SPYC;
+            self::$currentParser = $parser;
+        } elseif (self::$currentParser === null && extension_loaded('yaml')) {
+            self::$currentParser = self::PARSE_NATIVE;
+        } elseif (self::$currentParser === null) {
+            self::$currentParser = self::PARSE_SPYC;
         }
 
-        if (self::$parser === 'Spyc' && !class_exists('Spyc')) {
+        if (self::$currentParser === self::PARSE_SPYC && !class_exists('Spyc')) {
             if (self::$autoInstall) {
                 Tecnodesign_App_Install::dep('Spyc');
             } else {
@@ -64,59 +84,64 @@ class Tecnodesign_Yaml
             }
         }
 
-        return self::$parser;
-    }
+        if (self::$currentParser === self::PARSE_NATIVE && !extension_loaded('yaml')) {
+            throw new \RuntimeException('Yaml extension not installed.');
+        }
 
+        /**
+         * Just to maintain compatibility with someone extended this class
+         */
+        self::$parser = self::$currentParser;
+
+        return self::$currentParser;
+    }
 
     /**
      * Loads YAML text and converts to a PHP array
      *
-     * @param string $s file name or YAML string to load
+     * @param string $string file name or YAML string to load
      *
+     * @param int $cacheTimeout
      * @return array contents of the YAML text
      */
-    public static function load($s, $timeout = 1800)
+    public static function load($string, $cacheTimeout = 1800)
     {
-        $readTimeout = $timeout;
-        if ($s && strlen($s) < 255 && file_exists($s)) {
-            if (self::$parser === self::PARSE_NATIVE) {
-                $fn = 'yaml_parse_file';
-            } else {
-                $C = 'Spyc';
-                $fn = 'YAMLLoad';
-            }
-            $file = true;
-            $readTimeout = filemtime($s);
-        } else {
-            if (self::$parser === self::PARSE_NATIVE) {
-                $fn = 'yaml_parse';
-            } else {
-                $C = 'Spyc';
-                $fn = 'YAMLLoadString';
-            }
-            $file = false;
-        }
-        if (self::$cache && ($file || strlen($s) > 4000)) {
-            $ckey = 'yaml/' . md5($s);
-            $cache = Tecnodesign_Cache::get($ckey, $readTimeout);
-            if ($cache) {
-                return $cache;
-            }
-            if (isset($C)) {
-                $a = $C::$fn($s);
-            } else {
-                $a = $fn($s);
-            }
-            Tecnodesign_Cache::set($ckey, $a, $timeout);
-            unset($ckey, $cache, $fn, $readTimeout);
-            return $a;
-        } else {
-            if (isset($C)) {
-                return $C::$fn($s);
-            } else {
-                return $fn($s);
+        // Initialize the default parser
+        self::parser();
+
+        $readTimeout = $cacheTimeout;
+
+        $isFile = ($string && strlen($string) < 255 && file_exists($string));
+
+        $cacheKey = 'yaml/' . md5($string);
+        $useCache = self::$cache && ($isFile || strlen($string) > 4000);
+        if ($useCache) {
+            $cacheFound = Tecnodesign_Cache::get($cacheKey, $readTimeout);
+            if ($cacheFound) {
+                return $cacheFound;
             }
         }
+
+        $className = (self::$currentParser === self::PARSE_NATIVE)? null : 'Spyc';
+        if ($isFile) {
+            $functionName = (self::$currentParser === self::PARSE_NATIVE)? 'yaml_parse_file' : 'YAMLLoad';
+            $readTimeout = filemtime($string);
+        } else {
+            $functionName = (self::$currentParser === self::PARSE_NATIVE)? 'yaml_parse' : 'YAMLLoadString';
+        }
+
+        $yamlArray = $className ? $className::$functionName($string) : $functionName($string);
+
+        if ($useCache) {
+            Tecnodesign_Cache::set($cacheKey, $yamlArray, $cacheTimeout);
+        }
+
+        /**
+         * @todo necessary for PHP < 7
+         */
+        unset($cacheKey, $useCache, $className, $functionName, $readTimeout);
+
+        return $yamlArray;
     }
 
     /**
@@ -128,11 +153,11 @@ class Tecnodesign_Yaml
      */
     public static function loadString($s)
     {
-        if (self::$parser === self::PARSE_NATIVE) {
+        if (self::$currentParser === self::PARSE_NATIVE) {
             return yaml_parse($s);
-        } else {
-            return Spyc::YAMLLoadString($s);
         }
+
+        return Spyc::YAMLLoadString($s);
     }
 
     /**
