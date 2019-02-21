8383148d0dd1ba3baa656d572239daacc0407cb0
Rodrigo Capilé <rcapile@gmail.com>
2019-02-21 15:01:58 -0300

    Travis configuration

--- test/ci/install_dependencies.sh
+++ test/ci/install_dependencies.sh
@@ -5,7 +5,6 @@ if [[ ${phpversion} = '54' ]]; then
     echo "dependencies for PHP 5.4";
     sudo apt-get update && apt-get install -y libyaml-dev
     printf "\n" | pecl install yaml-1.3.2
-    echo "extension=yaml.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
     exit 0;
 fi;
 
@@ -14,7 +13,6 @@ if [[ ${phpversion} = '70' || ${phpversion} = '71' || ${phpversion} = '72' || ${
     sudo apt-get update \
         && apt-get install -y libyaml-dev
     printf "\n" | pecl install yaml
-    echo "extension=yaml.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
     exit 0;
 fi;
 
