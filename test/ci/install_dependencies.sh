eb6565f41bb0afcbcca44e15ef39701bfcdca84f
Rodrigo Capilé <rcapile@gmail.com>
2019-02-21 14:18:07 -0300

    Travis configuration

--- test/ci/install_dependencies.sh
+++ test/ci/install_dependencies.sh
@@ -2,6 +2,10 @@
 phpversion="$(php --version | head -n 1 | cut -d " " -f 2 | cut -c 1,3)";
 
 if [[ ${phpversion} = '54' ]]; then
+    echo "dependencies for PHP 7+";
+    #sudo apt-get update && apt-get install -y libyaml-dev
+    # printf "\n" | pecl install yaml-1.3.2
+    echo "extension=yaml.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
     exit 0;
 fi;
 
@@ -14,5 +18,5 @@ if [[ ${phpversion} = '70' || ${phpversion} = '71' || ${phpversion} = '72' || ${
     exit 0;
 fi;
 
-echo "dependencies not defined"
-exit 1;
+echo "No dependencies defined"
+exit 0;
