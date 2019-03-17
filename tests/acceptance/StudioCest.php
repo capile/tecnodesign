9aba31c9af8d022b743acef9346551e7cae4df8b
Guilherme Capilé <capile@tecnodz.com>
2019-03-17 12:07:30 -0300

    test css was generated and loaded in page

--- tests/acceptance/StudioCest.php
+++ tests/acceptance/StudioCest.php
@@ -4,7 +4,12 @@ class StudioCest
 {
     public function docsPageWorks(AcceptanceTester $I)
     {
+    	// remove cached css and see if it was properly generated
+    	$css = TDZ_DOCUMENT_ROOT.'/_/docs.css';
+    	if(file_exists($css)) unlink($css);
+
         $I->amOnPage('/docs/');
         $I->see('Tecnodesign Application Development Framework');  
+        $I->seeElement('link[href^="/_/docs.css?"]');
     }
 }
