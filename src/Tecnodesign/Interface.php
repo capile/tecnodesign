2da25e87bd0e24edb9ebe8a013aeac5ea6f92a46
Guilherme Capilé <capile@tecnodz.com>
2020-01-13 18:13:22 -0300

    -debug

--- src/Tecnodesign/Interface.php
+++ src/Tecnodesign/Interface.php
@@ -2687,7 +2687,6 @@ class Tecnodesign_Interface implements ArrayAccess
             unset($scope);
         }
         $this->text['list'] = $found;
-        \tdz::debug(__METHOD__, var_export($found, true), $cn::$schema->events);
         if(isset($this->options['list-key'])) $this->text['key'] = $this->options['list-key'];
         unset($found, $s, $start, $count, $cn, $order, $error);
         return $this->text['list'];
