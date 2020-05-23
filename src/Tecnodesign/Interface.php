111d5de43fd99f3713abb4d463b7169f3fc971c0
Guilherme Capilé <capile@tecnodz.com>
2020-05-23 15:54:53 -0300

    adjusting cache control directives

--- src/Tecnodesign/Interface.php
+++ src/Tecnodesign/Interface.php
@@ -1601,7 +1601,8 @@ class Tecnodesign_Interface implements ArrayAccess
         $title = $this->getTitle();
         if(!$this->action) {
             foreach(static::$actionsDefault as $a) {
-                if($this->setAction($a, tdz::urlParams(null, true))) {
+                $p1 = tdz::urlParams(null, true);
+                if($this->setAction($a, $p1)) {
                     break;
                 }
                 unset($a);
