2b271b184e2fb111bd5949c426d9622634dfc3af
Guilherme Capilé <capile@tecnodz.com>
2021-04-22 16:39:53 -0300

    oauth2 implementation (wip)

--- src/Studio/OAuth2/Server.php
+++ src/Studio/OAuth2/Server.php
@@ -12,6 +12,9 @@
 namespace Studio\OAuth2;
 
 use OAuth2\Request;
+use OAuth2\Response;
+use Studio\App;
+use tdz as S;
 
 class Server extends \OAuth2\Server
 {
@@ -20,7 +23,7 @@ class Server extends \OAuth2\Server
     {
         if(!static::$instance) {
             $storage = new Storage();
-            $app = \tdz::getApp()->studio;
+            $app = S::getApp()->studio;
             $cfg = ($app && isset($app['oauth2'])) ?$app['oauth2'] :[];
             $grantTypes = [];
             $responseTypes = [];
@@ -32,14 +35,14 @@ class Server extends \OAuth2\Server
             }
             if(isset($cfg['grant_types'])) {
                 foreach($cfg['grant_types'] as $i=>$o) {
-                    if(class_exists($cn = 'OAuth2\\GrantType\\'.\tdz::camelize($o, true))) {
+                    if(class_exists($cn = 'OAuth2\\GrantType\\'.S::camelize($o, true))) {
                         $grantTypes[$o] = new $cn($storage);
                     }
                 }
             }
             if(isset($cfg['response_types'])) {
                 foreach($cfg['response_types'] as $i=>$o) {
-                    if(class_exists($cn = 'OAuth2\\ResponseType\\'.\tdz::camelize($o, true))) {
+                    if(class_exists($cn = 'OAuth2\\ResponseType\\'.S::camelize($o, true))) {
                         $responseTypes[$o] = new $cn($storage);
                     }
                 }
@@ -49,11 +52,50 @@ class Server extends \OAuth2\Server
                 $cn = get_called_class();
                 static::$instance = new $cn($S, $cfg, $grantTypes, $responseTypes);
             } catch(\Exception $e) {
-                \tdz::debug(__METHOD__, var_export($e, true));
+                S::debug(__METHOD__, var_export($e, true));
             }
         }
 
         return static::$instance;
+    }
+
+    public static $routes=[
+        'access_token'=>'executeTokenRequest',
+        'auth'=>'executeAuth',
+        'authorize'=>'executeAuthorize',
+    ];
+
+    public static function app()
+    {
+        if(($route=App::response('route')) && isset($route['url'])) {
+            S::scriptName($route['url']);
+        }
+
+        if(($p = S::urlParams()) && count($p)==1 && isset(static::$routes[$p[0]])) {
+            $m = static::$routes[$p[0]];
+            if(is_array($m)) {
+                return S::exec($m);
+            } else {
+                return static::instance()->$m();
+            }
+        }
+
+        return App::error(404);
+    }
+
+    public static function appAccessToken()
+    {
+        static::instance()->executeTokenRequest();
+    }
+
+    public static function appAuth()
+    {
+        static::instance()->executeAuth();
+    }
+
+    public static function appAuthorize()
+    {
+        static::instance()->executeAuthorize();
     }
 
     public function executeTokenRequest()
@@ -62,7 +104,7 @@ class Server extends \OAuth2\Server
             $request = Request::createFromGlobals();
             $R = $this->handleTokenRequest($request);
         } catch(\Exception $e) {
-            \tdz::debug(__METHOD__, var_export($e, true));
+            S::debug(__METHOD__, var_export($e, true));
         }
         $R->send();
         exit();
@@ -76,6 +118,38 @@ class Server extends \OAuth2\Server
             die;
         }
 
-        \tdz::output(array('success' => true, 'message' => 'OK'), 'json');
+        S::output(array('success' => true, 'message' => 'OK'), 'json');
+    }
+
+    public function executeAuthorize()
+    {
+        $request = Request::createFromGlobals();
+        $response = new Response();
+
+        // validate the authorize request
+        if (!$this->validateAuthorizeRequest($request, $response)) {
+            $response->send();
+            die;
+        }
+        // display an authorization form
+        if (empty($_POST)) {
+          exit('
+        <form method="post">
+          <label>Do You Authorize TestClient?</label><br />
+          <input type="submit" name="authorized" value="yes">
+          <input type="submit" name="authorized" value="no">
+        </form>');
+        }
+
+        // print the authorization code if the user has authorized your client
+        $is_authorized = ($_POST['authorized'] === 'yes');
+        $this->handleAuthorizeRequest($request, $response, $is_authorized);
+        if ($is_authorized) {
+          // this is only here so that you get to see your code in the cURL request. Otherwise, we'd redirect back to the client
+          $code = substr($response->getHttpHeader('Location'), strpos($response->getHttpHeader('Location'), 'code=')+5, 40);
+          exit("SUCCESS! Authorization Code: $code");
+        }
+        $response->send();
+
     }
 }
