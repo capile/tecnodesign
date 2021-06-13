8737cf4c3945f03167bc844fba22a8af5ef6d929
Guilherme Capilé <capile@tecnodz.com>
2021-06-13 14:26:44 -0300

    fix ci

--- data/tests/bin/install_dependencies.sh
+++ data/tests/bin/install_dependencies.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 phpversion="$(php --version | head -n 1 | cut -d " " -f 2 | cut -c 1,3)";
 
-if [[ ${phpversion} = '54' || ${phpversion} = '55' || ${phpversion} = '56' ]]; then
-    echo "dependencies for PHP 5.4";
-    sudo apt-get update && sudo apt-get install -y libyaml-dev wget grep procps
-    printf "\n" | pecl install yaml-1.3.2
-    exit 0;
-fi;
-
 if [[ ${phpversion} = '70' || ${phpversion} = '71' || ${phpversion} = '72' ]]; then
     echo "dependencies for PHP 7+ (${phpversion})";
     sudo apt-get update \
@@ -16,8 +9,7 @@ if [[ ${phpversion} = '70' || ${phpversion} = '71' || ${phpversion} = '72' ]]; t
     exit 0;
 fi;
 
-if [[ ${phpversion} = '73' ]]; then
-    # PHP 7.3 dos not have a 7.3 version yet as of 25/fev/2019
+if [[ ${phpversion} = '73' || ${phpversion} = '74' || ${phpversion} = '80' ]]; then
     echo "dependencies for PHP 7.3 (${phpversion})";
     sudo apt-get update \
         && sudo apt-get install -y libyaml-dev wget grep procps
